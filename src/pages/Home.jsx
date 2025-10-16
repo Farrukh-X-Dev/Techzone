e79a4e0b1e57e602edf499d89c748b807f7ea14b
Farrukh-X-Dev <m.farrukhnoman@gmail.com>
2025-10-16 13:16:53 +0500

    Add Favicon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../components/Hero'
 import FAQs from '../components/FAQs '
 
 const Home = () => {
+  document.title = " Techzone Learning - Home "
   return (
     <>
       <Hero />
